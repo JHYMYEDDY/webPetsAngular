ed35e4c08829400d3eec75a17a34b115f33d02b9
YIMI <jhymiecanazaca10@gmail.com>
2019-05-22 09:58:00 -0500

    angular primero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { HomeComponent } from './view/home/home.component';
+import { EmpresaComponent } from './view/empresa/empresa.component';
+import { ProductosComponent } from './view/productos/productos.component';
+import { EspecialesComponent } from './view/especiales/especiales.component';
+import { ContactoComponent } from './view/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent,
+    HomeComponent,
+    EmpresaComponent,
+    ProductosComponent,
+    EspecialesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
